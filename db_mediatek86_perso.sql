2788a70bf9081c9728c41c280067ac232b320e02
yasser elkhoudiri <yasserparis0107@gmail.com>
2026-06-01 23:15:46 +0200

    Update db_mediatek86_perso.sql

--- db_mediatek86_perso.sql
+++ db_mediatek86_perso.sql
@@ -173,7 +173,9 @@ CREATE TABLE IF NOT EXISTS `responsable` (
 
 INSERT INTO `responsable` (`login`, `pwd`) VALUES
 ('responsable', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
-
+-- ----------------------------------------------------------------------
+-- mot de passe = 1234 (necessaire pour la connexion à l'application)
+-- ----------------------------------------------------------------------
 -- --------------------------------------------------------
 
 --
